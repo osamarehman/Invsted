52b6a1d254e244bf114158e89f1aae16ab5bf681
Usama Rehman <osamarehman@live.co.uk>
2023-06-14 15:22:29 +0500

    v2

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,12 +1,15 @@
 import redisClient from "../config/redisClient";
 
 export async function destroyAllActiveSessionsForUser(userId: string) {
-    let cursor = 0;
-    do {
-        const result = await redisClient.scan(cursor, { MATCH: `sess:${userId}*`, COUNT: 1000 });
-        for (const key of result.keys) {
-            await redisClient.del(key);
-        }
-        cursor = result.cursor;
-    } while (cursor !== 0);
+     let cursor = 0;
+     do {
+          const result = await redisClient.scan(cursor, {
+               MATCH: `sess:${userId}*`,
+               COUNT: 1000,
+          });
+          for (const key of result.keys) {
+               await redisClient.del(key);
+          }
+          cursor = result.cursor;
+     } while (cursor !== 0);
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,39 +1,45 @@
-import Footer from '@/components/Footer';
-import NavBar from '@/components/NavBar';
-import AuthModalsProvider from '@/components/auth/AuthModalsProvider';
-import useAuthenticatedUser from '@/hooks/useAuthenticatedUser';
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import AuthModalsProvider from "@/components/auth/AuthModalsProvider";
+import useAuthenticatedUser from "@/hooks/useAuthenticatedUser";
 import styles from "@/styles/App.module.css";
-import '@/styles/globals.scss';
-import '@/styles/utils.css';
-import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
+import "@/styles/globals.scss";
+import "@/styles/utils.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import NextNProgress from "nextjs-progressbar";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { Container, SSRProvider } from "react-bootstrap";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-
   useOnboardingRedirect();
 
   return (
     <>
       <Head>
-        <title>Flow Blog - Share your ideas</title>
-        <meta name="description" content="A full-stack NextJS course project by Coding in Flow" />
+        <title>Invsted - Share your ideas</title>
+        <meta
+          name="description"
+          content="A full-stack NextJS project by FluxFlow Digital"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" key="og:image" content="https://flowblog.com/social_media_preview_image.png" />
+        <meta
+          property="og:image"
+          key="og:image"
+          content="https://flowblog.com/social_media_preview_image.png"
+        />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <SSRProvider>
         <AuthModalsProvider>
           <div className={inter.className}>
-            <NextNProgress color='#21FA90' />
+            <NextNProgress color="#21FA90" />
             <NavBar />
             <main>
               <Container className={styles.pageContainer}>
